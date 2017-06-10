61e94077dce9426107f4723b6a79ac588463a4e7
Moirai01 <leah.restad@hotmail.com>
2017-06-10 14:19:32 -0500

    Add files via upload

---
+++ Peashooter.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package plusdungeon;
+
+/**
+ *
+ * @author leahr
+ */
+public class Peashooter extends Weapon {
+    
+    public Peashooter() {
+        super(0,5,0,0);
+    }
+    
+}
