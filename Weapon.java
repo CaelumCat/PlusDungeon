b15359a8257bfacb8d8f10356756187dd956a96c
Moirai01 <leah.restad@hotmail.com>
2017-06-09 20:53:17 -0500

    Add files via upload

---
+++ Weapon.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package plusdungeon;
+
+/**
+ *
+ * @author leahr
+ */
+public class Weapon {
+    int force; //damage from impact. staff, tackle
+    int pierce; //damage from stabbing or piercing. sword, gun
+    int slash; //damage from slashing. sword, claw
+    int magic; //damage by magic. staff, wand
+    //image
+    
+    public Weapon(int f, int p, int s, int m) {
+        force = f;
+        pierce = p;
+        slash = s;
+        magic = m;
+    }
+}
