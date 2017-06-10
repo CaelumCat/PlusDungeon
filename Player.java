fcce818fe72e6f0cce14369a52123cc2494a6904
Moirai01 <leah.restad@hotmail.com>
2017-06-09 20:51:41 -0500

    Add files via upload
    obviously this isn't complete. just if u wanna c what i have so far

---
+++ Player.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package plusdungeon;
+
+import java.util.ArrayList;
+
+
+/**
+ *
+ * @author leahr
+ */
+public class Player {
+    Weapon weapon;
+    ArrayList<Weapon> weapons;
+    Armor armor;
+    ArrayList<Armor> armors;
+    Potion potion;
+    ArrayList<Potion> potions;
+    int hp;
+    int mana;
+    
+    Player(int hp, int mana, Weapon weapon, gjg)
+    
+}
