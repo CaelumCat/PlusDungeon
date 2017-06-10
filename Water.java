5626e5a9aa6e8ac8827676a12de0596a4974c7d3
Moirai01 <leah.restad@hotmail.com>
2017-06-10 14:31:34 -0500

    Add files via upload

---
+++ Water.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package plusdungeon;
+
+/**
+ *
+ * @author Leah
+ * @author Christine
+ */
+public class Water extends Potion {
+    
+    public Water() {
+        super(0,0,0,0,0,0);
+    }
+    
+}
