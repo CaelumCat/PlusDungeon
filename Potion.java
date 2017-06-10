18107a70f9b6631871856f5c0b9f3f8d24b88cb6
Moirai01 <leah.restad@hotmail.com>
2017-06-10 14:10:37 -0500

    Update Potion.java

--- Potion.java
+++ Potion.java
@@ -5,10 +5,36 @@
  */
 package plusdungeon;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author leahr
  */
-public class Potion {
+public abstract class Potion {
+    ArrayList<Integer> effects = new ArrayList();
+    boolean usable = true;
+    
+    public Potion(int hp, int maxHP, int mana, int maxMana, int attack, int defense) {
+        effects.add(hp);
+        effects.add(maxHP);
+        effects.add(mana);
+        effects.add(maxMana);
+        effects.add(attack);
+        effects.add(defense);
+    }
+    
+    public ArrayList<Integer> getEffects() {
+        return effects;
+    }
+    
+    public ArrayList<Integer> useEffects() {
+        usable = false;
+        return effects;
+    }
+    
+    public void resetUse() {
+        usable = true;
+    }
     
 }
