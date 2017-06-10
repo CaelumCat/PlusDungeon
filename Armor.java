2cec354afb20cf8e6bbec3723d7816f75cf28949
Caelum <ddestinytail@gmail.com>
2017-06-09 21:22:34 -0500

    Cool edit

--- Armor.java
+++ Armor.java
@@ -4,11 +4,108 @@
  * and open the template in the editor.
  */
 package plusdungeon;
-
+import java.util.ArrayList;
 /**
  *
- * @author leahr
+ * @author Christine
  */
-public class Armor {
+public abstract class Player {
+    int hp;
+    int maxHP;
+    int mana;
+    int maxMana;
+    Weapon weapon;
+    ArrayList<Weapon> weapons;
+    Armor armor;
+    ArrayList<Armor> armors;
+    Potion potion;
+    ArrayList<Potion> potions;
+    
+    Player(int maxHP, int maxMana, Weapon weapon, Armor armor, Potion potion) {
+        this.maxHP = maxHP;
+        this.hp = maxHP;
+        this.maxMana = maxMana;
+        this.mana = maxMana;
+        this.weapon = weapon;
+        weapons.add(weapon);
+        this.armor = armor;
+        armors.add(armor);
+        this.potion = potion;
+        potions.add(potion);
+    }
+    
+    public int getHP() {
+        return hp;
+    }
+    
+    public void setHP(int newHP) {
+        if (newHP <= maxHP) {
+            hp = newHP;
+        } else {
+            hp = maxHP;
+        }
+    }
+    
+    public void setMaxHP(int newHP) {
+        maxHP = newHP;
+        if (hp > maxHP) {
+            hp = maxHP;
+        }
+    }
+    
+    public int getMana() {
+        return mana;
+    }
+    
+    public void setMana(int newMana) {
+        if (newMana <= maxMana) {
+            mana = newMana;
+        } else {
+            mana = maxMana;
+        }
+    }
+    
+    public void setMaxMana(int newMana) {
+        maxMana = newMana;
+        if (mana > maxMana) {
+            mana = maxMana;
+        }
+    }
+    
+    public Weapon getWeapon() {
+        return weapon;
+    }
+    
+    public void addWeapon(Weapon newWeapon) {
+        weapons.add(newWeapon);
+    }
+    
+    public void setWeapon(int newWeapon) {
+        weapon = weapons.get(newWeapon);
+    }
+    
+    public Armor getArmor() {
+        return armor;
+    }
+    
+    public void addArmor(Armor newArmor) {
+        armors.add(newArmor);
+    }
+    
+    public void setArmor(int newArmor) {
+        armor = armors.get(newArmor);
+    }
+    
+    public Potion getPotion() {
+        return potion;
+    }
+    
+    public void addPotion(Potion newPotion) {
+        potions.add(newPotion);
+    }
+    
+    public void setPotion(int newPotion) {
+        potion = potions.get(newPotion);
+    }
     
 }
