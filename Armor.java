c7dd8a969d8a0ffd4f0baa16949b2771cae3f934
Moirai01 <leah.restad@hotmail.com>
2017-06-10 14:17:13 -0500

    Update Armor.java
    (The code some hoe magically changed into Player code. Its good now.)

--- Armor.java
+++ Armor.java
@@ -4,108 +4,24 @@
  * and open the template in the editor.
  */
 package plusdungeon;
-import java.util.ArrayList;
+
 /**
  *
- * @author Christine
+ * @author leahr
  */
-public abstract class Player {
-    int hp;
-    int maxHP;
-    int mana;
-    int maxMana;
-    Weapon weapon;
-    ArrayList<Weapon> weapons;
-    Armor armor;
-    ArrayList<Armor> armors;
-    Potion potion;
-    ArrayList<Potion> potions;
+public abstract class Armor {
+    int force; //damage from impact. staff, tackle
+    int pierce; //damage from stabbing or piercing. sword, gun
+    int slash; //damage from slashing. sword, claw
+    int magic; //damage by magic. staff, wand
+    //image
+    //player position
     
-    Player(int maxHP, int maxMana, Weapon weapon, Armor armor, Potion potion) {
-        this.maxHP = maxHP;
-        this.hp = maxHP;
-        this.maxMana = maxMana;
-        this.mana = maxMana;
-        this.weapon = weapon;
-        weapons.add(weapon);
-        this.armor = armor;
-        armors.add(armor);
-        this.potion = potion;
-        potions.add(potion);
-    }
-    
-    public int getHP() {
-        return hp;
-    }
-    
-    public void setHP(int newHP) {
-        if (newHP <= maxHP) {
-            hp = newHP;
-        } else {
-            hp = maxHP;
-        }
-    }
-    
-    public void setMaxHP(int newHP) {
-        maxHP = newHP;
-        if (hp > maxHP) {
-            hp = maxHP;
-        }
-    }
-    
-    public int getMana() {
-        return mana;
-    }
-    
-    public void setMana(int newMana) {
-        if (newMana <= maxMana) {
-            mana = newMana;
-        } else {
-            mana = maxMana;
-        }
-    }
-    
-    public void setMaxMana(int newMana) {
-        maxMana = newMana;
-        if (mana > maxMana) {
-            mana = maxMana;
-        }
-    }
-    
-    public Weapon getWeapon() {
-        return weapon;
-    }
-    
-    public void addWeapon(Weapon newWeapon) {
-        weapons.add(newWeapon);
-    }
-    
-    public void setWeapon(int newWeapon) {
-        weapon = weapons.get(newWeapon);
-    }
-    
-    public Armor getArmor() {
-        return armor;
-    }
-    
-    public void addArmor(Armor newArmor) {
-        armors.add(newArmor);
-    }
-    
-    public void setArmor(int newArmor) {
-        armor = armors.get(newArmor);
-    }
-    
-    public Potion getPotion() {
-        return potion;
-    }
-    
-    public void addPotion(Potion newPotion) {
-        potions.add(newPotion);
-    }
-    
-    public void setPotion(int newPotion) {
-        potion = potions.get(newPotion);
+    public Armor(int f, int p, int s, int m) {
+        force = f;
+        pierce = p;
+        slash = s;
+        magic = m;
     }
     
 }
