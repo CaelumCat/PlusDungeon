8a9adc417684d0ce4d36695c87b62153784822f8
Moirai01 <leah.restad@hotmail.com>
2017-06-10 14:21:54 -0500

    Add files via upload
    This is Cat's starting armor. IDK if the numbers are good, but they sounded pathetic enough

---
+++ Fur.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package plusdungeon;
+
+/**
+ *
+ * @author leahr
+ */
+public class Fur extends Armor {
+    
+    public Fur() {
+        super(2,2,2,0);
+    }
+    
+}
