becfa3c220bdacdd42ef70d5f631ac29b3e7e9c2
Moirai01 <leah.restad@hotmail.com>
2017-06-09 20:55:25 -0500

    Update Player.java

--- Player.java
+++ Player.java
@@ -12,16 +12,103 @@ import java.util.ArrayList;
  *
  * @author leahr
  */
-public class Player {
+public abstract class Player {
+    int hp;
+    int maxHP;
+    int mana;
+    int maxMana;
     Weapon weapon;
     ArrayList<Weapon> weapons;
     Armor armor;
     ArrayList<Armor> armors;
     Potion potion;
     ArrayList<Potion> potions;
-    int hp;
-    int mana;
     
-    Player(int hp, int mana, Weapon weapon, gjg)
+    Player(int hp, int mana, Weapon weapon, Armor armor, Potion potion) {
+        this.hp = hp;
+        maxHP = hp;
+        this.mana = mana;
+        maxMana = mana;
+        this.weapon = weapon;
+        weapons.add(weapon);
+        this.armor = armor;
+        armors.add(armor);
+        this.potion = potion;
+        potions.add(potion);
+    }
+    
+    public int getHP() {
+        return hp;
+    }
+    
+    public void setHP(int newHP) {
+        if (newHP <= maxHP) {
+            hp = newHP;
+        } else {
+            hp = maxHP;
+        }
+    }
+    
+    public void setMaxHP(int newHP) {
+        maxHP = newHP;
+        if (hp > maxHP) {
+            hp = maxHP;
+        }
+    }
+    
+    public int getMana() {
+        return mana;
+    }
+    
+    public void setMana(int newMana) {
+        if (newMana <= maxMana) {
+            mana = newMana;
+        } else {
+            mana = maxMana;
+        }
+    }
+    
+    public void setMaxMana(int newMana) {
+        maxMana = newMana;
+        if (mana > maxMana) {
+            mana = maxMana;
+        }
+    }
+    
+    public Weapon getWeapon() {
+        return weapon;
+    }
+    
+    public void addWeapon(Weapon newWeapon) {
+        weapons.add(newWeapon);
+    }
+    
+    public void setWeapon(int newWeapon) {
+        weapon = weapons.get(newWeapon);
+    }
+    
+    public Armor getArmor() {
+        return armor;
+    }
+    
+    public void addArmor(Armor newArmor) {
+        armors.add(newArmor);
+    }
+    
+    public void setArmor(int newArmor) {
+        armor = armors.get(newArmor);
+    }
+    
+    public Potion getPotion() {
+        return potion;
+    }
+    
+    public void addPotion(Potion newPotion) {
+        potions.add(newPotion);
+    }
+    
+    public void setPotion(int newPotion) {
+        potion = potions.get(newPotion);
+    }
     
 }
