2dc91b26fdb9c8041df4164aab732fea7d6fb900
Moirai01 <leah.restad@hotmail.com>
2017-06-11 10:08:04 -0500

    Update Enemy.java
    Obviously take damage isn't finished but I anted you to c my updates

--- Enemy.java
+++ Enemy.java
@@ -9,6 +9,38 @@ package plusdungeon;
  *
  * @author leahr
  */
-public class Enemy {
+public abstract class Enemy {
+    double hp;
+    Weapon weapon;
+    Armor armor;
+    double attackPer = 1;
+    double defensePer = 1;
+    
+    public Enemy(double hp, Weapon w, Armor a) {
+        this.hp = hp;
+        weapon = w;
+        armor = a;
+    }
+    
+    public void attack(Player p) {
+        p.takeDamage(0,weapon.getForce()*attackPer);
+        p.takeDamage(1,weapon.getPierce()*attackPer);
+        p.takeDamage(2,weapon.getSlash()*attackPer);
+        p.takeDamage(3,weapon.getMagic()*attackPer);
+    }
+    
+    public void takeDamage(int attackType, double damage) {
+        if(attackType == 0) {
+            double forceDamage = armor.getForce()*defensePer - damage;
+            if(forceDamage > 0) {
+                hp -= forceDamage;
+            }
+        } else if (attackType == 1) {
+            double pierceDamage = armor.getPierce()*defensePer - damage;
+            if (pierceDamage > 0) {
+                
+            }
+        }
+    }
     
 }
